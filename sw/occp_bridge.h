5eb1a4160a1a6f502b9c9447474c53124b7bd6f4
math-code <mathcode220@gmail.com>
2026-08-23 18:09:04 +0300

    Add header file for OCCP bridge definitions

---
+++ sw/occp_bridge.h
@@ -0,0 +1,30 @@
+#ifndef OCCP_BRIDGE_H
+#define OCCP_BRIDGE_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#define OCCP_BASE_ADDR      0x40000000UL
+#define OCCP_CTRL_REG       (OCCP_BASE_ADDR + 0x00)
+#define OCCP_STATUS_REG     (OCCP_BASE_ADDR + 0x04)
+#define OCCP_SRAM_SKEW_A    (OCCP_BASE_ADDR + 0x10)
+#define OCCP_SRAM_SKEW_B    (OCCP_BASE_ADDR + 0x20)
+#define OCCP_SRAM_RESULT    (OCCP_BASE_ADDR + 0x30)
+
+#define OCCP_CMD_START      0x01
+#define OCCP_CMD_RESET      0x02
+
+#define OCCP_STATUS_READY   0x01
+#define OCCP_STATUS_BUSY    0x02
+#define OCCP_STATUS_DONE    0x04
+#define OCCP_STATUS_ERROR   0x08
+
+#define OCCP_TIMEOUT_CYCLES 10000000
+#define OCCP_MATRIX_SIZE    2
+
+int occp_init(void);
+int occp_dispatch_matrix_multiply(const float *matrix_A, const float *matrix_B, float *matrix_out);
+int occp_reset(void);
+int occp_is_hardware_available(void);
+
+#endif
